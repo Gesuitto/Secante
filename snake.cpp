e636707a3aa3ef5738265626b34781c86a7ab2f2
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-04-29 19:42:21 +0200

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -6,85 +6,76 @@
 #include <stdlib.h>
 using namespace std;
 // g++ snake.cpp -o snake.exe -std=c++2a
+std::string SNAKE="$";
+std::string BORDER="#";
+std::string APPLE="0";
 
 
 bool gameover(string matrix[20][20]) {
-    bool controllo=true;
-    for (int i = 0; i <19; i++)
-    {
-        for (int j = 0; j <19; j++)
-        if(matrix[i][0] == "a" ||     matrix[i][19] == "a" || matrix[0][j]=="a" || matrix[19][j]=="a")
-        {
-            controllo=false;
-        }
+    for (int i = 0; i <20; i++) {
+        if (matrix[i][0] == SNAKE || matrix[i][19] == SNAKE)
+            return false;
     }
-    return controllo;
+    for (int j = 0; j<20; j++) {
+        if (matrix[0][j]==SNAKE || matrix[19][j]==SNAKE)
+            return false;
+    }
+    return true;
 }
 
 
-// Se colpisci una mela allora ritorni true
-bool controllomela1(string campo[20][20], int riga, int colonna)
-{
-    if(campo[riga][colonna]=="O")
-        return true;
-    else
-        return false;
-}
-void nuovamela(string campo[20][20])
-{
-    campo[rand()%18][rand()%18]="O";
-    // campo[7][12]="O";
+bool appleCheck(string campo[20][20], int x, int y) {
+    return (campo[x][y] == APPLE);
 }
 
 
-void movimento(string matrix[][20], char direzione, int & x, int & y)
-{
-    // Ripristino il bordo
-    for (int i = 0; i <20; i++)
-    {
-        for (int j = 0; j <20; j++)
-        {
-            if(matrix[i][j]=="a")
+void addApple(string campo[20][20], int x, int y) {
+    int a, b;
+    do {
+        a = 1+rand()%17;
+        b = 1+rand()%17;
+    } while (a == x && b == y);
+    campo[a][b] = APPLE;
+}
+
+
+void movimento(string matrix[][20], char direzione, int & x, int & y) {
+    for (int i = 0; i <20; i++) {
+        for (int j = 0; j <20; j++) {
+            if(matrix[i][j]==SNAKE)
                 matrix[i][j]=" ";
         }
-        matrix[i][0] = "#";
-        matrix[i][19] = "#";
+        matrix[i][0] = BORDER;
+        matrix[i][19] = BORDER;
     }
     matrix[x][y] = " ";
     for (int i = 0; i < 20; i++)
     {
-        matrix[0][i]="#";
-        matrix[19][i]="#";
+        matrix[0][i]=BORDER;
+        matrix[19][i]=BORDER;
     }
 
     // Muovo a seconda della direzione
-    if(direzione=='w')
-    {
+    if(direzione=='w') {
         x--;
-        if(controllomela1(matrix, x, y)==true)
-            nuovamela(matrix);
-        matrix[x][y]="a";
-    }
-    else if(direzione=='s')
-    {
+        if(appleCheck(matrix, x, y)==true)
+            addApple(matrix, x, y);
+        matrix[x][y]=SNAKE;
+    } else if(direzione=='s') {
         x++;
-        if(controllomela1(matrix, x, y)==true)
-            nuovamela(matrix);
-        matrix[x][y]="a";
-    }
-    else if(direzione=='a')
-    {
+        if(appleCheck(matrix, x, y)==true)
+            addApple(matrix, x, y);
+        matrix[x][y]=SNAKE;
+    } else if(direzione=='a') {
         y--;
-        if(controllomela1(matrix, x, y)==true)
-            nuovamela(matrix);
-        matrix[x][y]="a";
-    }
-    else if(direzione=='d')
-    {
+        if(appleCheck(matrix, x, y)==true)
+            addApple(matrix, x, y);
+        matrix[x][y]=SNAKE;
+    } else if(direzione=='d') {
         y++;
-        if(controllomela1(matrix, x, y)==true)
-            nuovamela(matrix);
-        matrix[x][y]="a";
+        if(appleCheck(matrix, x, y)==true)
+            addApple(matrix, x, y);
+        matrix[x][y]=SNAKE;
     }
 }
 
@@ -117,35 +108,30 @@ void cambiaDirezione(char & direzione, string mossa) {
 }
 
 
-int main()
-{
+int main() {
     srand(time(NULL));
     string campo[20][20];
     int x=9;
     int y=9;
     char direzione='w'; // Variabile che ha il valore w quando si muove in alto, s quando si muove in basso, a quando si muove a sinistra, d quando si muove a destra
-    for (int i = 0; i <20; i++)
-    {
+    for (int i = 0; i <20; i++) {
         for (int j = 0; j <20; j++)
             campo[i][j]=" ";
     }
-    for(int i=0; i<20;i++)
-    {
-        campo[0][i]="#";
-        campo[i][0]="#";
-        campo[19][i]="#";
-        campo[i][19]="#";
-        campo[x][y]="a";
+    for(int i=0; i<20;i++) {
+        campo[0][i]=BORDER;
+        campo[i][0]=BORDER;
+        campo[19][i]=BORDER;
+        campo[i][19]=BORDER;
+        campo[x][y]=SNAKE;
     }
 
     // Genero la prima mela
-    nuovamela(campo);
+    addApple(campo, x, y);
     stampa(campo);
     string num;
 
-    while(gameover(campo)==true) //cin
-    {
-    	// Eseguo la funzione inputMove in modo asincrono
+    while(gameover(campo)==true) {
 	    auto input = std::async(std::launch::async, inputMossa);
         system("cls");
 		using namespace std::literals;
@@ -153,8 +139,7 @@ int main()
 			movimento(campo, direzione, x, y);
             stampa(campo);
 		}
-		num = input.get(); // Assegnamo a num la variabile presa da inputMossa in maniera asincrona
+		num = input.get();
         cambiaDirezione(direzione, num);
     }
-    cout<<"gameover";   
 }
