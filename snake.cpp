7c869c12d9870f6112ef186adefcf7a1fb2fffc2
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-04-30 20:49:18 +0200

    System options
    System options

--- snake.cpp
+++ snake.cpp
@@ -2,12 +2,14 @@
 #include <thread>
 #include <future>
 #include <chrono>
+#include <cstring>
 #include <time.h>
 // g++ snake.cpp -o snake.exe -std=c++2a
 // g++ -S snake.cpp -std=c++2a
 const char SNAKE = '$';
 const char BORDER = '#';
 const char APPLE = '0';
+const std::string VERSION = "v0.3.0";
 
 
 void gameover(char matrix[20][20], unsigned long long points) {
@@ -81,7 +83,36 @@ void changeDirection(char & direction, std::string move) {
 }
 
 
-int main() {
+int main(int argc, char *argv[]) {
+    float seconds = 0.2;
+    if (argc == 1) {
+        std::cout << "Period [seconds]: ";
+        std::cin >> seconds;
+    } else if (argc == 2 && !strcmp(argv[1], "--help")) {
+        std::cout << "Usage: snake [--help] [--version] [--frequency | --delay]" << std::endl << std::endl;
+        std::cout << "Options:" << std::endl;
+        std::cout << "  --help         Show this help message and exit" << std::endl;
+        std::cout << "  --version      Show version and exit" << std::endl;
+        std::cout << "  --frequency    Set the frame period in seconds" << std::endl;
+        std::cout << "  --delay        Set the frame period in seconds" << std::endl;
+        std::cout << "Examples:" << std::endl;
+        std::cout << "  snake --help" << std::endl;
+        std::cout << "  snake --frequency 0.2" << std::endl;
+        std::cout << "  snake --delay 0.2" << std::endl;
+        return 0;
+    } else if (argc == 2 && !strcmp(argv[1], "--version")) {
+        std::cout << VERSION << std::endl;
+        return 0;
+    } else if (argc == 3 && !strcmp(argv[1], "--frequency")) {
+        seconds = atof(argv[2]);
+    } else if (argc == 3 && !strcmp(argv[1], "--delay")) {
+        seconds = atof(argv[2]);
+    } else if (argc > 2) {
+        std::cout << "Usage: snake [--help] [--version] [--frequency | --delay]" << std::endl;
+        return 0;
+    }
+    std::chrono::duration<double> period(seconds);
+
     srand(time(NULL));
     unsigned long long points = 0;
     char field[20][20];
@@ -104,8 +135,7 @@ int main() {
 
     while (true) {
 	    auto input = std::async(std::launch::async, inputMove);
-		using namespace std::literals;
-		while (input.wait_for(0.2s) != std::future_status::ready) {
+		while (input.wait_for(period) != std::future_status::ready) {
 			move(field, direction, x, y, points);
             print(field);
 		}
